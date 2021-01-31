28412d5d52f25746ef18c4230d15178174745661
Andrej Lončík <andrejloncik@gmail.com>
2021-01-31 15:03:31 +0100

    Add ingredients section to discover layout

--- app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/discover/DiscoverFragment.kt
@@ -1,6 +1,8 @@
 package com.lonchi.andrej.lonchi_bakalarka.ui.discover
 
 import android.view.View
+import androidx.core.content.ContextCompat
+import coil.load
 import com.lonchi.andrej.lonchi_bakalarka.R
 import com.lonchi.andrej.lonchi_bakalarka.databinding.FragmentDiscoverBinding
 import com.lonchi.andrej.lonchi_bakalarka.ui.base.BaseFragment
@@ -18,9 +20,41 @@ class DiscoverFragment : BaseFragment<DiscoverViewModel, FragmentDiscoverBinding
     override val bindingInflater: (View) -> FragmentDiscoverBinding = { FragmentDiscoverBinding.bind(it) }
 
     override fun initView() {
-        binding?.labelDiscover?.setOnClickListener {
-            viewModel.getRandomRecipes()
-            //viewModel.getRecipeDetail()
+        binding?.ingredient1?.imageIngredient?.load(ContextCompat.getDrawable(requireContext(), R.drawable.img_avocado)) {
+            placeholder(R.color.gray50)
+            error(R.color.gray50)
+        }
+        binding?.ingredient2?.imageIngredient?.load(ContextCompat.getDrawable(requireContext(), R.drawable.img_cheese)) {
+            placeholder(R.color.gray50)
+            error(R.color.gray50)
+        }
+        binding?.ingredient3?.imageIngredient?.load(ContextCompat.getDrawable(requireContext(), R.drawable.img_red_onion)) {
+            placeholder(R.color.gray50)
+            error(R.color.gray50)
+        }
+        binding?.ingredient4?.imageIngredient?.load(ContextCompat.getDrawable(requireContext(), R.drawable.img_avocado)) {
+            placeholder(R.color.gray50)
+            error(R.color.gray50)
+        }
+        binding?.ingredient5?.imageIngredient?.load(ContextCompat.getDrawable(requireContext(), R.drawable.img_cheese)) {
+            placeholder(R.color.gray50)
+            error(R.color.gray50)
+        }
+        binding?.ingredient6?.imageIngredient?.load(ContextCompat.getDrawable(requireContext(), R.drawable.img_red_onion)) {
+            placeholder(R.color.gray50)
+            error(R.color.gray50)
+        }
+        binding?.ingredient7?.imageIngredient?.load(ContextCompat.getDrawable(requireContext(), R.drawable.img_avocado)) {
+            placeholder(R.color.gray50)
+            error(R.color.gray50)
+        }
+        binding?.ingredient8?.imageIngredient?.load(ContextCompat.getDrawable(requireContext(), R.drawable.img_cheese)) {
+            placeholder(R.color.gray50)
+            error(R.color.gray50)
+        }
+        binding?.ingredient9?.imageIngredient?.load(ContextCompat.getDrawable(requireContext(), R.drawable.img_red_onion)) {
+            placeholder(R.color.gray50)
+            error(R.color.gray50)
         }
     }
 
