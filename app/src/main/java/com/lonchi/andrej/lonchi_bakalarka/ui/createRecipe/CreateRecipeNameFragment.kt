57171330b867277663719c523a035ef9f01b1a7c
Andrej Lončík <andrejloncik@gmail.com>
2021-03-13 13:33:33 +0100

    Create search input

--- app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/createRecipe/CreateRecipeNameFragment.kt
+++ app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/createRecipe/CreateRecipeNameFragment.kt
@@ -1,10 +1,13 @@
 package com.lonchi.andrej.lonchi_bakalarka.ui.createRecipe
 
 import android.view.View
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.lonchi.andrej.lonchi_bakalarka.R
 import com.lonchi.andrej.lonchi_bakalarka.databinding.FragmentCreateRecipeNameBinding
+import com.lonchi.andrej.lonchi_bakalarka.logic.util.hideKeyboard
 import com.lonchi.andrej.lonchi_bakalarka.ui.base.BaseFragment
+import timber.log.Timber
 
 /**
  * @author Andrej Lončík <andrejloncik@gmail.com>
@@ -25,7 +28,20 @@ class CreateRecipeNameFragment : BaseFragment<CreateRecipeNameViewModel, Fragmen
         binding?.buttonNext?.setOnClickListener {
             findNavController().navigate(CreateRecipeNameFragmentDirections.actionNameFragmentToTimeFragment())
         }
+
+        binding?.searchInput?.setEndIconClickClearInput(true)
+        binding?.searchInput?.setMicrophoneIconOnClickListener {
+            requireActivity().hideKeyboard()
+            Toast.makeText(requireContext(), "hlasa ze majk", Toast.LENGTH_SHORT).show()
+        }
+        binding?.searchInput?.requestFocus()
     }
 
-    override fun bindViewModel() = Unit
+    override fun bindViewModel() {
+        //binding?.searchInput?.changeCurrentQuery(viewModel.filterString.value.orEmpty())
+        binding?.searchInput?.setTextObserver {
+            binding?.buttonNext?.isEnabled = it.isNotEmpty()
+            Timber.d("bindViewModel: .searchInput?.setTextObserver = $it")
+        }
+    }
 }
