32c246cecd94f538ab09cfb221fb88476bc1bc67
Andrej Lončík <andrejloncik@gmail.com>
2021-03-19 22:29:35 +0100

    Connect own recipes screen to real data big boy glep

--- app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/ownRecipes/OwnRecipesFragment.kt
+++ app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/ownRecipes/OwnRecipesFragment.kt
@@ -1,9 +1,17 @@
 package com.lonchi.andrej.lonchi_bakalarka.ui.ownRecipes
 
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lonchi.andrej.lonchi_bakalarka.R
+import com.lonchi.andrej.lonchi_bakalarka.data.entities.RecipeCustom
+import com.lonchi.andrej.lonchi_bakalarka.data.entities.RecipeFavourite
+import com.lonchi.andrej.lonchi_bakalarka.data.entities.RecipeItem
 import com.lonchi.andrej.lonchi_bakalarka.databinding.FragmentOwnRecipesBinding
 import com.lonchi.andrej.lonchi_bakalarka.ui.base.BaseFragment
+import com.lonchi.andrej.lonchi_bakalarka.ui.discover.RecipeRowsAdapter
+import com.lonchi.andrej.lonchi_bakalarka.ui.home.HomeFragmentDirections
+import com.lonchi.andrej.lonchi_bakalarka.ui.recipe_detail.RecipeIdTypeEnum
 
 
 /**
@@ -20,10 +28,44 @@ class OwnRecipesFragment : BaseFragment<OwnRecipesViewModel, FragmentOwnRecipesB
     override val bindingInflater: (View) -> FragmentOwnRecipesBinding =
         { FragmentOwnRecipesBinding.bind(it) }
 
-    override fun initView() {
-        binding?.iconBack?.setOnClickListener { requireActivity().onBackPressed() }
-        binding?.chipCounter?.text = (0..10).random().toString()
+    private val adapterRecipes by lazy {
+        RecipeRowsAdapter(
+            context = requireContext(),
+            onRecipeClick = { onRecipeClick(it) }
+        )
     }
 
-    override fun bindViewModel() = Unit
+    override fun initView() {
+        binding?.iconBack?.setOnClickListener { requireActivity().onBackPressed() }
+
+        binding?.recyclerRecipes?.adapter = adapterRecipes
+        binding?.recyclerRecipes?.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+    }
+
+    override fun bindViewModel() {
+        viewModel.getAllCustomRecipes().observe(viewLifecycleOwner) {
+            handleRecipes(it)
+        }
+    }
+
+    private fun handleRecipes(recipes: List<RecipeCustom>) {
+        binding?.chipCounter?.text = recipes.size.toString()
+
+        if (recipes.isNullOrEmpty()) {
+            //  TODO - add empty state
+        } else {
+            //  TODO - hide added empty state
+            adapterRecipes.submitList(recipes)
+        }
+    }
+
+    private fun onRecipeClick(recipe: RecipeItem) {
+        findNavController().navigate(
+            HomeFragmentDirections.actionGlobalRecipeDetailFragment(
+                recipeId = recipe.getId(),
+                idType = RecipeIdTypeEnum.FAVOURITE_RECIPE
+            )
+        )
+    }
 }
