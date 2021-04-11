5776841585eb4454e6604407464531de78aa55e3
Andrej Lončík <andrejloncik@gmail.com>
2021-04-11 23:41:42 +0200

    Add dark text to status bar and change launcher icons

--- app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/base/BaseActivity.kt
+++ app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/base/BaseActivity.kt
@@ -73,6 +73,10 @@ abstract class BaseActivity<T, V : ViewBinding> : DaggerAppCompatActivity() wher
 
         handleSavedInstanceState(savedInstanceState)
 
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
         initView()
         bindViewModel()
     }
