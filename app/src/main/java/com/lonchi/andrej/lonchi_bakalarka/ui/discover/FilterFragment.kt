e9f24f7a710e7cdd2c4928e89db79acab254917e
Andrej Lončík <andrejloncik@gmail.com>
2021-04-14 00:27:54 +0200

    Finish filter UI

--- app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/discover/FilterFragment.kt
+++ app/src/main/java/com/lonchi/andrej/lonchi_bakalarka/ui/discover/FilterFragment.kt
@@ -19,7 +19,49 @@ class FilterFragment : BaseFragment<FilterViewModel, FragmentFilterBinding>() {
 
     override fun initView() {
         binding?.iconBack?.setOnClickListener { requireActivity().onBackPressed() }
+        binding?.iconReset?.setOnClickListener { requireActivity().onBackPressed() }
+        binding?.buttonSave?.setOnClickListener { requireActivity().onBackPressed() }
+
+        setupRanges()
     }
 
     override fun bindViewModel() = Unit
+
+    private fun setupRanges() {
+        binding?.rangeCalories?.addOnChangeListener { rangeSlider, _, _ ->
+            rangeSlider.values.getOrNull(0)?.let {
+                binding?.textCaloriesMin?.text = getString(R.string.filter_gram_value, it.toInt())
+            }
+            rangeSlider.values.getOrNull(1)?.let {
+                binding?.textCaloriesMax?.text = getString(R.string.filter_gram_value, it.toInt())
+            }
+        }
+
+        binding?.rangeProtein?.addOnChangeListener { rangeSlider, _, _ ->
+            rangeSlider.values.getOrNull(0)?.let {
+                binding?.textProteinMin?.text = getString(R.string.filter_gram_value, it.toInt())
+            }
+            rangeSlider.values.getOrNull(1)?.let {
+                binding?.textProteinMax?.text = getString(R.string.filter_gram_value, it.toInt())
+            }
+        }
+
+        binding?.rangeFat?.addOnChangeListener { rangeSlider, _, _ ->
+            rangeSlider.values.getOrNull(0)?.let {
+                binding?.textFatMin?.text = getString(R.string.filter_gram_value, it.toInt())
+            }
+            rangeSlider.values.getOrNull(1)?.let {
+                binding?.textFatMax?.text = getString(R.string.filter_gram_value, it.toInt())
+            }
+        }
+
+        binding?.rangeCarbs?.addOnChangeListener { rangeSlider, _, _ ->
+            rangeSlider.values.getOrNull(0)?.let {
+                binding?.textCarbsMin?.text = getString(R.string.filter_gram_value, it.toInt())
+            }
+            rangeSlider.values.getOrNull(1)?.let {
+                binding?.textCarbsMax?.text = getString(R.string.filter_gram_value, it.toInt())
+            }
+        }
+    }
 }
